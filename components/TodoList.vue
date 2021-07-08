4a415c7b33d6d57e6e722a39e1a230d89067e13b
MilttO0nMss <grountish@gmail.com>
2021-07-08 18:02:22 +0200

    todolist v1

---
+++ components/TodoList.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="w-11/12 mx-auto my-5">
+    <AddTodo @AddTodo="AddTodo" />
+    <div class="text-center border">
+      <div class="border flex justify-between">
+        <div class="border w-28">Date</div>
+        <div class="">Description</div>
+        <div class="border w-28">Actions</div>
+      </div>
+      <div v-for="todo in todos" :key="todo.id">
+        <Todo :todo="todo" @DeleteTodo="DeleteTodo" @CheckTodo="CheckTodo" />
+      </div>
+    </div>
+/******************************
+ * summary here               *
+ ******************************/
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todos: [],
+    }
+  },
+  methods: {
+    AddTodo(todo) {
+      this.todos.push(todo)
+    },
+    DeleteTodo(todoToDelete) {
+      let tods = this.todos.filter((todo) => todo.id != todoToDelete.id)
+
+      this.todos = tods
+    },
+    CheckTodo(todoToCheck) {
+      this.todos.forEach((todo) => {
+        if (todo.id == todoToCheck.id) {
+          todo.check = todoToCheck.check
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <Tutorial/>
+  <TodoList/>
 </template>
 
 <script>
